d20754d212b8b8e1c8a0023421196bf7831e19c5
Callum <Callum_nzl@live.com>
2023-06-12 18:11:22 +1200

    added random tunnel generator with two.js

--- creative/src/TwoExample.js
+++ creative/src/TwoExample.js
@@ -10,13 +10,61 @@ function TwoExample() {
       autostart: true,
     }).appendTo(ref.current)
 
-    const circle = two.makeCircle(two.width / 2, two.height / 2, 50)
-    circle.fill = '#FF8000'
-    circle.stroke = 'orangered'
+    let clickPosition = new Two.Vector(two.width / 2, two.height / 2)
+    const colors = ['green', 'black', 'red', 'blue', 'yellow', 'white']
 
-    two.bind('update', function () {
-      circle.rotation += 0.001
-    })
+    // Flag indicating whether the user has clicked
+    let hasUserClicked = false
+
+    function generateBorderPosition() {
+      const side = Math.floor(Math.random() * 4)
+      switch (side) {
+        default:
+          return new Two.Vector(Math.random() * two.width, 0)
+        case 0: // top
+          return new Two.Vector(Math.random() * two.width, 0)
+        case 1: // right
+          return new Two.Vector(two.width, Math.random() * two.height)
+        case 2: // bottom
+          return new Two.Vector(Math.random() * two.width, two.height)
+        case 3: // left
+          return new Two.Vector(0, Math.random() * two.height)
+      }
+    }
+
+    setInterval(() => {
+      // Only draw lines if the user has clicked
+      if (hasUserClicked) {
+        const borderPosition = generateBorderPosition()
+        const line = two.makeLine(
+          borderPosition.x,
+          borderPosition.y,
+          clickPosition.x,
+          clickPosition.y
+        )
+        line.stroke = colors[Math.floor(Math.random() * colors.length)]
+        line.linewidth = 1
+      }
+    }, 0.1)
+
+    two.renderer.domElement.addEventListener(
+      'click',
+      function (e) {
+        // Set the flag to true when the user clicks
+        hasUserClicked = true
+      },
+      false
+    )
+
+    // Update clickPosition every time the mouse moves
+    two.renderer.domElement.addEventListener(
+      'mousemove',
+      function (e) {
+        const rect = two.renderer.domElement.getBoundingClientRect()
+        clickPosition.set(e.clientX - rect.left, e.clientY - rect.top)
+      },
+      false
+    )
   }, [])
 
   return <div ref={ref} />
